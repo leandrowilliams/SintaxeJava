49969708873d004535b4142fb49de0ad1172ef77
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-08-27 21:26:24 -0300

    Initial commit (Primitive Types)

---
+++ src/main/java/com/example/sintaxejava/SintaxeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sintaxejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintaxeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintaxeJavaApplication.class, args);
+	}
+
+}
