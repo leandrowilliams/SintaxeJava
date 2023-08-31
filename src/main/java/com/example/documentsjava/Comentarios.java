bb0fb64d312fec79ee817cf1d056b934c86878c7
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-08-31 00:47:54 -0300

    AboutMe final

--- src/main/java/com/example/documentsjava/Comentarios.java
+++ src/main/java/com/example/documentsjava/Comentarios.java
@@ -29,15 +29,15 @@ public class Comentarios {
  * dois números
  */
 
-public int somaMultiplica (int n, int x, String m){
-
-    int r = 0; // r é igual ao resultado
-    if (m == "M"){ // M= multiplicação
-        r= n * x;
-    }else{
-        // se não soma mesmo
-        r = n + x
-    }
-    return r;
-}
+//public int somaMultiplica (int n, int x, String m){
+//
+//    int r = 0; // r é igual ao resultado
+//    if (m == "M"){ // M= multiplicação
+//        r= n * x;
+//    }else{
+//        // se não soma mesmo
+//        r = n + x
+//    }
+//    return r;
+//}
 }}
