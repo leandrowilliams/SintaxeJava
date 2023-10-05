a8aba71035ffb081eee25e08d279175989c49129
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-10-05 12:24:35 -0300

    altura de João e Marcos, comparando > or <

---
+++ src/main/java/com/example/controlefluxo/AlturaPessoa.java
@@ -0,0 +1,17 @@
+package com.example.controlefluxo;
+
+public class AlturaPessoa {
+
+    public static void main(String[] args) {
+        double alturaJoao = 1.46;
+        double alturaMarcos = 1.37;
+
+        if (alturaJoao > alturaMarcos) {
+            System.out.println("João é mais alto que Marcos");
+        } else {
+            System.out.println("Marcos é mais baixo que João");
+        }
+    }
+
+
+}
