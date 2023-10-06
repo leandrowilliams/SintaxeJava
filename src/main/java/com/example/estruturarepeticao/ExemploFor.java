4d7876f34958c45af20415c79540ace7cc6fd68c
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-10-05 23:48:05 -0300

    for and inicialização, validação, atualização

---
+++ src/main/java/com/example/estruturarepeticao/ExemploFor.java
@@ -0,0 +1,11 @@
+package com.example.estruturarepeticao;
+
+public class ExemploFor {
+
+    public static void main(String[] args) {
+        for(int carneirinhos = 1 ; carneirinhos <=50; carneirinhos ++) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+        }
+    }
+
+}
