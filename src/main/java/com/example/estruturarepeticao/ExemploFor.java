89e44dbb4b7a32c7e50f79f69697c48b1c7c4254
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-10-06 23:32:07 -0300

    estruturas

--- src/main/java/com/example/estruturarepeticao/ExemploFor.java
+++ src/main/java/com/example/estruturarepeticao/ExemploFor.java
@@ -6,6 +6,28 @@ public class ExemploFor {
         for(int carneirinhos = 1 ; carneirinhos <=50; carneirinhos ++) {
             System.out.println(carneirinhos + " - Carneirinho(s)");
         }
+
+        System.out.println(" ------------------------ ");
+
+        // Outras estruturas
+
+        //estrutura 1
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+        }
+
+        System.out.println(" ------------------------ ");
+
+        //estrutura 2
+        //o que importa é somente o bloco condicional
+        int carneirinhos = 1;
+        for( ; carneirinhos <=20; ) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+            carneirinhos ++;
+        }
+
+        //for( somente 1x ; deve ser uma expresão boolean; acontecerá a cada final da execução ) { }
+
     }
 
 }
