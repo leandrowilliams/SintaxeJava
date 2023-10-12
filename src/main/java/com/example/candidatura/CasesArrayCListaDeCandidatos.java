0da58a94c42945e53b96184e13c3aa9964f4586a
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-10-12 13:45:32 -0300

    fazendo ligacoes para cada candidato selecionado, com sucesso e sem sucesso, for, do while, if and else

--- src/main/java/com/example/candidatura/CasesArrayCListaDeCandidatos.java
+++ src/main/java/com/example/candidatura/CasesArrayCListaDeCandidatos.java
@@ -1,11 +1,46 @@
 package com.example.candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CasesArrayCListaDeCandidatos {
 
     public static void main(String[] args) {
 //        selecaoCandidatos();
-        imprimirSelecionados();
+//        imprimirSelecionados();
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        for(String candidato : candidatos) {
+            entrandoEmContato(candidato);
+
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu=false;
+        do {
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("=============================");
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        }while(continuarTentando && tentativasRealizadas<3);
+
+        if(atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+        else
+            System.out.println("===================================================================");
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+
+
+    }
+
+    //método auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3)==1;
     }
 
     static void imprimirSelecionados(){
