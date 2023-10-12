f208507a87958806ce4358912a3352d0b804cb3d
leandrowilliams <dev.leandrowilliams@outlook.com>
2023-10-11 23:35:26 -0300

    imprimindo com indice e for each

--- src/main/java/com/example/candidatura/CasesArrayCListaDeCandidatos.java
+++ src/main/java/com/example/candidatura/CasesArrayCListaDeCandidatos.java
@@ -4,7 +4,22 @@ import java.util.concurrent.ThreadLocalRandom;
 public class CasesArrayCListaDeCandidatos {
 
     public static void main(String[] args) {
-        selecaoCandidatos();
+//        selecaoCandidatos();
+        imprimirSelecionados();
+    }
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        for (int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de numero " + (indice+1) + " é " + candidatos[indice]);
+        }
+        System.out.println("======================================");
+        System.out.println("Forma abreviada de interação For Each:");
+
+        for(String candidato: candidatos) {
+            System.out.println("O candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidatos(){
@@ -13,7 +28,7 @@ public class CasesArrayCListaDeCandidatos {
         int candidatosSelecionados = 0;
         int candidatosAtual = 0;
         double salarioBasePadrao = 2000.0;
-        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+        while(candidatosSelecionados < 10 && candidatosAtual < candidatos.length) {
             String candidato = candidatos [candidatosAtual];
             double salarioPretendido = valorPretendido();
 
